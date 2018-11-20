454b1495df90f451e93c4d89b9f3d0d0492a72c4
vaslee <ltx0503@gmail.com>
2018-11-20 15:32:21 -0500

    first try

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userInputTextField: UITextField!
+    @IBOutlet weak var cohortTextField: UITextField!
+    
+    
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    // step 3: set textfield to self
+    userInputTextField.delegate = self
+    cohortTextField.delegate = self
   }
 
-
 }
+// step: 1: conform to UITextFiledDelegate
+extension ViewController: UITextFieldDelegate {
+    // step 2: implement methods as needed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // dismiss keyboard
+        textField.resignFirstResponder()
+        
+        if textField == cohortTextField {
+            // do cohort logic
+            print("cohort textField")
+        }
+
+        print("user entered \(userInputTextField.text ?? "nothing entered")")
+        // does not matter here is true or false
+        // since textField behaviour is on one line
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(textField.text ?? "")
+        return true
+    }
+}
+
+
 
